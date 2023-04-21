4ebf9395594ab0efd9779b856aeaa93c6237d69c
joeteck <adeyojuibukunoluwa1@gmail.com>
2023-04-21 12:57:46 +0000

    progress2

--- forntend/src/pages/login.jsx
+++ forntend/src/pages/login.jsx
@@ -3,6 +3,7 @@ import {login_image, view, metalogo} from '../assets';
 
 import { useState } from "react";
 import {Navbar, Footer2, } from '../components';
+import { redirect } from "react-router-dom";
 
 function ShowPassword() {
   var x = document.getElementById("password");
@@ -58,68 +59,76 @@ const login = ({state, account}) => {
     event.preventDefault();
     const {contract} = state;
 
-    const data1 = await contract.getUsers();
-    console.log("Users:", data1)
-    setData(data1);
-    console.log("Users:", data)
-    alert("Gotten Users Successfully.")
+    try{
+      const data1 = await contract.getUsers();
+      if (data1){
+        console.log("Users:", data1)
+        console.log("Users:", data)
+      }
+      setData(data1);
+      alert("Gotten Users Successfully.")
+    }catch(error)
+    {
+      
+      console.log("Error")
+    }
   }
 
   return (
   
-  <div className="bg-primary w-full overflow-hidden">
-    <div className= {`${styles.paddingX} ${styles.flexCenter}`}>
-      <div className={`${styles.boxWidth}`}>
-      <Navbar />
-      </div>
-    </div>
-
-    <section id='home' className={`flex flex-col items-center md:flex-row text-center`}>
-      <div className={`flex flex-col ${styles.flexCenter} bg-primary ${styles.boxWidth}`}>
-
-        <div className={`flex-1 flex  ${styles.flexCenter} md:my-0 my-10 relative`}>
-          <img src={login_image} alt="Robot Hand Image" className="w-[400px] h-[250px] relative z-[5]"/>
-
-          <div className='absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient'></div>
-          <div className='absolute z-[1] w-[80%] h-[80%] bottom-40 white__gradient'></div>
-          <div className='absolute z-[0] w-[50%] h-[50%] right-40 bottom-40 blue__gradient'></div>
-       </div>
-        <p className={` ${styles.paragraph} py-5`}>
-        You can enter private key to your wallet here. Or enter your Metamask wallet
-        </p>
-
-        {/* ===========FORM=================== */}
-        <form action={`#`} method="" className={`w-full ${styles.flexCenter} flex flex-col`}>
-        <div className={`flex flex-row bg-white px-[20px] py-[5px] border-radius mb-5 border-div border-dimWhite sm:w-[50%] md:w-[38%] w-[70%]`}>
-          <input id="password" type="password" className={`text-[18px] font-poppins letterSpacing w-[100%] focus:outline-0`}  placeholder="Private Key"/>
-          <img src={view} alt="Hid Password" className={`w-[30px] h-[30px]`} onClick={ShowPassword}/>
+    <div className="bg-primary w-full overflow-hidden">
+      <div className= {`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+        <Navbar />
         </div>
-        <div className={`${styles.flexCenter} flex flex-col`}>
-          <button id="login-btn" className={`login-btn`}>
-            Continue
-          </button>
-          <p className={` ${styles.paragraph}`}>
-            Or Click to Continue with Metamask <br />
-            {/* {ethBalance} */}
-            Connected account : {account}
+      </div>
+
+      <section id='home' className={`flex flex-col items-center md:flex-row text-center`}>
+        <div className={`flex flex-col ${styles.flexCenter} bg-primary ${styles.boxWidth}`}>
+
+          <div className={`flex-1 flex  ${styles.flexCenter} md:my-0 my-10 relative`}>
+            <img src={login_image} alt="Robot Hand Image" className="w-[400px] h-[250px] relative z-[5]"/>
+
+            <div className='absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient'></div>
+            <div className='absolute z-[1] w-[80%] h-[80%] bottom-40 white__gradient'></div>
+            <div className='absolute z-[0] w-[50%] h-[50%] right-40 bottom-40 blue__gradient'></div>
+        </div>
+          <p className={` ${styles.paragraph} py-5`}>
+          You can enter private key to your wallet here. Or enter your Metamask wallet
           </p>
-              <button id="meta-btn" className={`meta-btn mb-[100px]`} onClick={checkUser} >
-                <img src={metalogo} alt="Meta Logo" className={`meta-img`}/>
-                <p className={`meta-p`}>MetaMask</p>
-              </button>
-          {/* {
-            !isConnected && (
-              
-            )
-          } */}
+
+          {/* ===========FORM=================== */}
+          <form action={`#`} method="" className={`w-full ${styles.flexCenter} flex flex-col`}>
+          <div className={`flex flex-row bg-white px-[20px] py-[5px] border-radius mb-5 border-div border-dimWhite sm:w-[50%] md:w-[38%] w-[70%]`}>
+            <input id="password" type="password" className={`text-[18px] font-poppins letterSpacing w-[100%] focus:outline-0`}  placeholder="Private Key"/>
+            <img src={view} alt="Hid Password" className={`w-[30px] h-[30px]`} onClick={ShowPassword}/>
+          </div>
+          <div className={`${styles.flexCenter} flex flex-col`}>
+            <button id="login-btn" className={`login-btn`}>
+              Continue
+            </button>
+            <p className={` ${styles.paragraph}`}>
+              Or Click to Continue with Metamask <br />
+              {/* {ethBalance} */}
+              Connected account : {account}
+            </p>
+                <button id="meta-btn" className={`meta-btn mb-[100px]`} onClick={checkUser} >
+                  <img src={metalogo} alt="Meta Logo" className={`meta-img`}/>
+                  <p className={`meta-p`}>MetaMask</p>
+                </button>
+            {/* {
+              !isConnected && (
+                
+              )
+            } */}
+          </div>
+          </form>
         </div>
-        </form>
-      </div>
-    </section>
-    <Footer2 />
-  </div>
-);
-  }
+      </section>
+      <Footer2 />
+    </div>
+  );
+}
 
 export default login
 
